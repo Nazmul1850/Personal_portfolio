5cc70985a03bbbeb882ac428192309ae96793f5a
Nazmul Hasan <nazmul.buet17@gmail.com>
2020-04-13 00:32:09 +0600

    again settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -125,6 +125,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 try:
-    from .local_settings import *
-except ImprtError:
+    from local_settings import *
+except ImportError:
     print("Looks like no local file")
